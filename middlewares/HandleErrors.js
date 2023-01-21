d3ad8393c55d5f7c4be5311bae16dc767f5d2a06
Dmytro Shakh <work.dmytro02@gmail.com>
2023-01-21 14:53:56 +0100

    fix: переместить вызов next в конец

--- middlewares/HandleErrors.js
+++ middlewares/HandleErrors.js
@@ -3,9 +3,9 @@ const { MESSAGE, STATUS } = require('../utils/constants');
 module.exports.handleErrors = ((err, req, res, next) => {
   const { statusCode = STATUS.ERROR.SERVER, message } = err;
 
-  next();
-
   res
     .status(statusCode)
     .send({ message: statusCode === STATUS.ERROR.SERVER ? MESSAGE.ERROR.SERVER : message });
+
+  next();
 });
